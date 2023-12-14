5a5a1c5e12c4c9f34ae10f5595a90cfd23db7f71
Alexander Nicholson <alexn400@gmail.com>
2023-12-14 16:01:44 -0500

    Add password protection

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import * as basicAuth from 'express-basic-auth';
+
+const SWAGGER_PATH = '/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Note: It's important that this comes BEFORE calling SwaggerModule.setup
+  app.use(
+      [SWAGGER_PATH, `${SWAGGER_PATH}-json`],
+      basicAuth({
+        challenge: true,
+        users: {
+          ['admin']: 'admin',
+        },
+      }),
+  );
 
   const config = new DocumentBuilder()
       .setTitle('Cats example')
@@ -11,9 +25,9 @@ async function bootstrap() {
       .setVersion('1.0')
       .addTag('cats')
       .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
 
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(SWAGGER_PATH, app, document);
 
   await app.listen(3000);
 }
