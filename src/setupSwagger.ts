b8f58392dfda90e9c0f6beca0cc495467652f9f8
Alexander Nicholson <alexn400@gmail.com>
2023-12-15 15:08:46 -0500

    Simplify code

--- src/setupSwagger.ts
+++ src/setupSwagger.ts
@@ -3,32 +3,32 @@ import {ConfigService} from "@nestjs/config";
 import * as basicAuth from "express-basic-auth";
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
-const SWAGGER_PATH = '/swagger'
-
 /**
  * Sets up password-protected swagger documentation for the application
  */
 export const setupSwagger = (app: INestApplication) => {
-    const swaggerPassword = app.get(ConfigService).get('SWAGGER_PASSWORD');
+  const swaggerPassword = app.get(ConfigService).get('SWAGGER_PASSWORD');
 
-    // Note: It's important that this comes BEFORE calling SwaggerModule.setup()
+  // Note: It's important that this comes BEFORE calling SwaggerModule.setup()
+  if (process.env.NODE_ENV !== 'development') {
     app.use(
-        [SWAGGER_PATH, `${SWAGGER_PATH}-json`],
-        basicAuth({
-            challenge: true,
-            users: {
-                admin: swaggerPassword,
-            },
-        }),
+      ['/docs', '/docs-json'],
+      basicAuth({
+        challenge: true,
+        users: {
+          admin: swaggerPassword,
+        },
+      }),
     );
+  }
 
-    const config = new DocumentBuilder()
-        .setTitle('Cats example')
-        .setDescription('The cats API description')
-        .setVersion('1.0')
-        .addTag('cats')
-        .build();
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
 
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup(SWAGGER_PATH, app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, document);
 }
